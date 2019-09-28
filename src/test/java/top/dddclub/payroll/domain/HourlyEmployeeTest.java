5f3e02de39817f9045f35fa45c06c224bfdfd98b
agiledon <15023157626@163.com>
2019-09-28 22:08:15 +0800

    extract parameter for createTimeCards(); write a new test for overtime case

--- src/test/java/top/dddclub/payroll/domain/HourlyEmployeeTest.java
+++ src/test/java/top/dddclub/payroll/domain/HourlyEmployeeTest.java
@@ -12,7 +12,7 @@ public class HourlyEmployeeTest {
     @Test
     public void should_calculate_payroll_by_work_hours_in_a_week() {
         //given
-        List<TimeCard> timeCards = createTimeCards();
+        List<TimeCard> timeCards = createTimeCards(8, 8, 8, 8, 8);
         Money salaryOfHour = Money.of(10000, Currency.RMB);
         HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
 
@@ -26,12 +26,29 @@ public class HourlyEmployeeTest {
         assertThat(payroll.amount()).isEqualTo(Money.of(400000, Currency.RMB));
     }
 
-    private List<TimeCard> createTimeCards() {
-        TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 9, 2), 8);
-        TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 9, 3), 8);
-        TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 9, 4), 8);
-        TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 9, 5), 8);
-        TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 9, 6), 8);
+    @Test
+    public void should_calculate_payroll_by_work_hours_with_overtime_in_a_week() {
+        //given
+        List<TimeCard> timeCards = createTimeCards(9, 7, 10, 10, 8);
+        Money salaryOfHour = Money.of(10000, Currency.RMB);
+        HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
+
+        //when
+        Payroll payroll = hourlyEmployee.payroll();
+
+        //then
+        assertThat(payroll).isNotNull();
+        assertThat(payroll.beginDate()).isEqualTo(LocalDate.of(2019, 9, 2));
+        assertThat(payroll.endDate()).isEqualTo(LocalDate.of(2019, 9, 6));
+        assertThat(payroll.amount()).isEqualTo(Money.of(465000, Currency.RMB));
+    }
+
+    private List<TimeCard> createTimeCards(int workHours1, int workHours2, int workHours3, int workHours4, int workHours5) {
+        TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 9, 2), workHours1);
+        TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 9, 3), workHours2);
+        TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 9, 4), workHours3);
+        TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 9, 5), workHours4);
+        TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 9, 6), workHours5);
 
         List<TimeCard> timeCards = new ArrayList<>();
         timeCards.add(timeCard1);
