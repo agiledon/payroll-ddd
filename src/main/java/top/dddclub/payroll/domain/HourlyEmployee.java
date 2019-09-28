137e6cb06768a7e1aab65ad3f9078620f91243eb
agiledon <15023157626@163.com>
2019-09-28 21:48:16 +0800

    refactor payroll() method

--- src/main/java/top/dddclub/payroll/domain/HourlyEmployee.java
+++ src/main/java/top/dddclub/payroll/domain/HourlyEmployee.java
@@ -1,5 +1,6 @@
 package top.dddclub.payroll.domain;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,8 +18,27 @@ public class HourlyEmployee {
                 .map(tc -> tc.workHours())
                 .reduce(0, (hours, total) -> hours + total);
 
+        return new Payroll(
+                settlementPeriod().beginDate,
+                settlementPeriod().endDate,
+                salaryOfHour.multiply(totalHours));
+    }
+
+    private Period settlementPeriod() {
         Collections.sort(timeCards);
 
-        return new Payroll(timeCards.get(0).workDay(), timeCards.get(timeCards.size() - 1).workDay(), salaryOfHour.multiply(totalHours));
+        LocalDate beginDate = timeCards.get(0).workDay();
+        LocalDate endDate = timeCards.get(timeCards.size() - 1).workDay();
+        return new Period(beginDate, endDate);
+    }
+
+    private class Period {
+        private LocalDate beginDate;
+        private LocalDate endDate;
+
+        Period(LocalDate beginDate, LocalDate endDate) {
+            this.beginDate = beginDate;
+            this.endDate = endDate;
+        }
     }
 }
