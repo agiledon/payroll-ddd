7cb1cfaa38a879997856ee47c766367b451b60f9
agiledon <zhangyi1101@tom.com>
2019-10-01 15:20:33 +0800

    let failed test pass

--- src/main/java/top/dddclub/payroll/domain/HourlyEmployeePayrollCalculator.java
+++ src/main/java/top/dddclub/payroll/domain/HourlyEmployeePayrollCalculator.java
@@ -1,13 +1,20 @@
 package top.dddclub.payroll.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HourlyEmployeePayrollCalculator {
-    public void setRepository(HourlyEmployeeRepository employeeRepository) {
+    private HourlyEmployeeRepository employeeRepository;
 
+    public void setRepository(HourlyEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     public List<Payroll> execute(Period settlementPeriod) {
-        return null;
+        List<HourlyEmployee> hourlyEmployees = employeeRepository.allEmployeesOf(settlementPeriod);
+        return hourlyEmployees.stream()
+                .map(e -> e.payroll(settlementPeriod))
+                .collect(Collectors.toList());
     }
 }
