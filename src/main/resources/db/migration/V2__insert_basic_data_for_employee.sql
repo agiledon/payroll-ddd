7ecb933ebf41557fa8a3aa88724f5373768357f5
agiledon <zhangyi1101@tom.com>
2019-10-07 11:07:40 +0800

    insert employees sample data

---
+++ src/main/resources/db/migration/V2__insert_basic_data_for_employee.sql
@@ -0,0 +1,10 @@
+insert into employees (id, name, email, employeeType, gender, country, province, city, street, zip, mobilePhone, homePhone, officePhone)
+ values ("emp200190101000001", 'Bruce', 'bruce@payroll.com', 0, 'male', 'China', 'SiChuan', 'chengdu', 'qingyang avenue', '600000', '15028150000', null, null);
+ insert into employees (id, name, email, employeeType, gender, country, province, city, street, zip, mobilePhone, homePhone, officePhone)
+ values ("emp200190101000002", 'Tom', 'tom@payroll.com', 0, 'male', 'China', 'Beijing', 'beijing', 'chaoyang changan avenue', '100000', '13910150000', null, null);
+ insert into employees (id, name, email, employeeType, gender, country, province, city, street, zip, mobilePhone, homePhone, officePhone)
+ values ("emp200190101000003", 'Rose', 'rose@payroll.com', 1, 'female', 'China', 'Beijing', 'beijing', 'chaoyang xidan avenue', '100000', '15010123456', null, null);
+ insert into employees (id, name, email, employeeType, gender, country, province, city, street, zip, mobilePhone, homePhone, officePhone)
+ values ("emp200190101000004", 'Zhang Yi', 'zhangyi@payroll.com', 2, 'male', 'China', 'SiChuan', 'chengdu', 'chengfei avenue', '600001', '15028151234', null, null);
+ insert into employees (id, name, email, employeeType, gender, country, province, city, street, zip, mobilePhone, homePhone, officePhone)
+ values ("emp200190101000005", 'Huang Rong', 'huangrong@payroll.com', 2, 'female', 'China', 'SiChuan', 'chengdu', 'chengfei avenue', '600001', '15028159999', null, null);
