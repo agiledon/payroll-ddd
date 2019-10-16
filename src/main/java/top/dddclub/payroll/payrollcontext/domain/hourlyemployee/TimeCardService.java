333101f88ed779c9d72b0da1e60d542c596ce628
agiledon <zhangyi1101@tom.com>
2019-10-16 09:34:22 +0800

    let failed test pass

--- src/main/java/top/dddclub/payroll/payrollcontext/domain/hourlyemployee/TimeCardService.java
+++ src/main/java/top/dddclub/payroll/payrollcontext/domain/hourlyemployee/TimeCardService.java
@@ -1,17 +1,20 @@
 package top.dddclub.payroll.payrollcontext.domain.hourlyemployee;
 
 import top.dddclub.payroll.employeecontext.domain.EmployeeId;
+import java.util.Optional;
 
 public class TimeCardService {
-    private HourlyEmployeeRepository hourlyEmployeeRepository;
+    private HourlyEmployeeRepository employeeRepository;
 
-    public void setHourlyEmployeeRepository(HourlyEmployeeRepository hourlyEmployeeRepository) {
-
-        this.hourlyEmployeeRepository = hourlyEmployeeRepository;
+    public void setEmployeeRepository(HourlyEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     public void submitTimeCard(EmployeeId employeeId, TimeCard submitted) {
-
-
+        Optional<HourlyEmployee> optEmployee = employeeRepository.employeeOf(employeeId);
+        optEmployee.ifPresent(e -> {
+            e.submit(submitted);
+            employeeRepository.save(e);
+        });
     }
 }
--- src/test/java/top/dddclub/payroll/payrollcontext/domain/hourlyemployee/TimeCardServiceTest.java
+++ src/test/java/top/dddclub/payroll/payrollcontext/domain/hourlyemployee/TimeCardServiceTest.java
@@ -8,8 +8,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class TimeCardServiceTest {
     private String employeeId = "emp200901011111";
@@ -17,20 +16,23 @@ public class TimeCardServiceTest {
     @Test
     public void should_submit_time_card() {
         //given
+        EmployeeId employeeIdObj = EmployeeId.of(employeeId);
+
         TimeCardService timeCardService = new TimeCardService();
 
         HourlyEmployeeRepository mockRepo = mock(HourlyEmployeeRepository.class);
         HourlyEmployee hourlyEmployee = EmployeeFixture.hourlyEmployeeOf(employeeId, null);
-        when(mockRepo.employeeOf(EmployeeId.of(employeeId))).thenReturn(Optional.of(hourlyEmployee));
-
-        timeCardService.setHourlyEmployeeRepository(mockRepo);
+        when(mockRepo.employeeOf(employeeIdObj)).thenReturn(Optional.of(hourlyEmployee));
+        timeCardService.setEmployeeRepository(mockRepo);
 
         TimeCard newTimeCard = new TimeCard(LocalDate.of(2019, 10, 8), 8);
 
         //when
-        timeCardService.submitTimeCard(EmployeeId.of(employeeId), newTimeCard);
+        timeCardService.submitTimeCard(employeeIdObj, newTimeCard);
 
         //then
+        verify(mockRepo).employeeOf(employeeIdObj);
+        verify(mockRepo).save(hourlyEmployee);
         assertThat(hourlyEmployee.timeCards()).hasSize(1);
     }
 }
