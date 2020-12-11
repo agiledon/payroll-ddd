231d6bce0a04166988200df64b68705a6919b53d
zhangyi <brucezhang@tom.com>
2020-12-11 13:39:50 +0800

    yi - refactor the first test for PunchingRecord

--- src/test/java/top/dddclub/payroll/employeecontext/domain/PunchingRecordTest.java
+++ src/test/java/top/dddclub/payroll/employeecontext/domain/PunchingRecordTest.java
@@ -12,20 +12,27 @@ public class PunchingRecordTest {
     @Test
     public void should_acquire_normal_status() {
         // given
-        LocalTime startTime = LocalTime.of(9, 0, 0);
-        LocalTime endTime = LocalTime.of(17, 0, 0);
-        WorkHour workHour = new WorkHour(startTime, endTime);
-
-        String employeeId = "emp0001";
-        LocalDateTime punchingIn = LocalDateTime.of(2020, 12, 10, 9, 0, 0);
-        LocalDateTime punchingOut = LocalDateTime.of(2020, 12, 10, 17, 0, 0);
-        LocalDate today = LocalDate.of(2020, 12, 10);
-        PunchingRecord punching = new PunchingRecord(employeeId, punchingIn, punchingOut, today);
+        WorkHour workHour = createWorkHour();
+        PunchingRecord punching = createPunchingRecord();
 
         // when
         AttendanceStatus status = punching.acquireStatus(workHour);
 
         // then
         assertThat(status.isNormal()).isTrue();
+    }
+
+    private PunchingRecord createPunchingRecord() {
+        String employeeId = "emp0001";
+        LocalDateTime punchingIn = LocalDateTime.of(2020, 12, 10, 9, 0, 0);
+        LocalDateTime punchingOut = LocalDateTime.of(2020, 12, 10, 17, 0, 0);
+        LocalDate today = LocalDate.of(2020, 12, 10);
+        return new PunchingRecord(employeeId, punchingIn, punchingOut, today);
+    }
+
+    private WorkHour createWorkHour() {
+        LocalTime startTime = LocalTime.of(9, 0, 0);
+        LocalTime endTime = LocalTime.of(17, 0, 0);
+        return new WorkHour(startTime, endTime);
     }
 }
